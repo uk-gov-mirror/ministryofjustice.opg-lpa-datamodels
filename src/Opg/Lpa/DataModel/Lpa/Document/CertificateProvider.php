313140f7258df5d6b78572481a5a67e3f8641e4e
Neil Smith <neil.smith@digital.justice.gov.uk>
2014-11-12 11:44:21 +0000

    Added Payment, Notified Person and Cert Provider classes.

---
+++ src/Opg/Lpa/DataModel/Lpa/Document/CertificateProvider.php
@@ -0,0 +1,28 @@
+<?php
+namespace Opg\Lpa\DataModel\Lpa\Document;
+
+use Opg\Lpa\DataModel\Lpa\AbstractData;
+use Opg\Lpa\DataModel\Lpa\Document\Elements;
+
+use Respect\Validation\Rules;
+use Opg\Lpa\DataModel\Validator\Validator;
+
+class CertificateProvider extends AbstractData {
+
+    protected $name;
+    protected $address;
+
+
+    public function __construct(){
+        parent::__construct();
+
+        $this->name = new Elements\Name();
+        $this->address = new Elements\Address();
+
+        //-----------------------------------------------------
+        // Validators (wrapped in Closures for lazy loading)
+
+
+    } // function
+
+} // class
