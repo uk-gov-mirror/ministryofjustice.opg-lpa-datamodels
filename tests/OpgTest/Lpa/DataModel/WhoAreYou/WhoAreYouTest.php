ad43d4dff334c56e694543abe3b41c32537e6b96
David Winchurch <david.winchurch@iterativeit.com>
2017-07-05 09:48:19 +0100

    LPA-2305: 100% coverage on WhoAreYou

--- tests/OpgTest/Lpa/DataModel/WhoAreYou/WhoAreYouTest.php
+++ tests/OpgTest/Lpa/DataModel/WhoAreYou/WhoAreYouTest.php
@@ -2,7 +2,45 @@
 
 namespace OpgTest\Lpa\DataModel\WhoAreYou;
 
+use Opg\Lpa\DataModel\WhoAreYou\WhoAreYou;
+
 class WhoAreYouTest extends \PHPUnit_Framework_TestCase
 {
+    public function testValidationFailed()
+    {
+        $whoAreYou = new WhoAreYou();
 
+        $validatorResponse = $whoAreYou->validate();
+        $this->assertTrue($validatorResponse->hasErrors());
+        $errors = $validatorResponse->getArrayCopy();
+        $this->assertEquals(1, count($errors));
+        $this->assertNotNull($errors['who']);
+    }
+
+    public function testValidationFailedWhoSet()
+    {
+        $whoAreYou = new WhoAreYou();
+        $whoAreYou->set('who', 'Test');
+
+        $validatorResponse = $whoAreYou->validate();
+        $this->assertTrue($validatorResponse->hasErrors());
+        $errors = $validatorResponse->getArrayCopy();
+        $this->assertEquals(1, count($errors));
+        $this->assertNotNull($errors['who']);
+    }
+
+    public function testValidationFailedIncorrectChoice()
+    {
+        $whoAreYou = new WhoAreYou();
+        $whoAreYou->set('who', 'donor');
+        $whoAreYou->set('subquestion', 'Incorrect');
+        $whoAreYou->set('qualifier', true);
+
+        $validatorResponse = $whoAreYou->validate();
+        $this->assertTrue($validatorResponse->hasErrors());
+        $errors = $validatorResponse->getArrayCopy();
+        $this->assertEquals(2, count($errors));
+        $this->assertNotNull($errors['subquestion']);
+        $this->assertNotNull($errors['qualifier']);
+    }
 }
