ce3fbbc64c9a567d83cc2d9100178f41ed17e057
David Winchurch <david.winchurch@iterativeit.com>
2017-07-06 17:40:43 +0100

    LPA-2305: Code review changes

--- src/Opg/Lpa/DataModel/AbstractData.php
+++ src/Opg/Lpa/DataModel/AbstractData.php
@@ -251,9 +251,11 @@ abstract class AbstractData implements AccessorInterface, JsonSerializable, Vali
      */
     public function toArray($dateFormat = 'string')
     {
+        // @codeCoverageIgnoreStart
         if ($dateFormat == 'mongo' && !class_exists('\MongoDate')) {
             throw new InvalidArgumentException('You not have the PHP Mongo extension installed');
         }
+        // @codeCoverageIgnoreEnd
 
         $values = get_object_vars($this);
 
