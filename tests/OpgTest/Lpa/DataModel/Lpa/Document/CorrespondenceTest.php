94e9872fdc1a5b9c5f8ee4063ae102a0e0a9dbe2
David Winchurch <david.winchurch@iterativeit.com>
2017-09-19 09:42:27 +0100

    LPA-2293: Fixing unit tests

--- tests/OpgTest/Lpa/DataModel/Lpa/Document/CorrespondenceTest.php
+++ tests/OpgTest/Lpa/DataModel/Lpa/Document/CorrespondenceTest.php
@@ -59,8 +59,9 @@ class CorrespondenceTest extends \PHPUnit_Framework_TestCase
         $validatorResponse = $correspondence->validate();
         $this->assertTrue($validatorResponse->hasErrors());
         $errors = $validatorResponse->getArrayCopy();
-        $this->assertEquals(3, count($errors));
+        $this->assertEquals(4, count($errors));
         TestHelper::assertNoDuplicateErrorMessages($errors, $this);
+        $this->assertNotNull($errors['name']);
         $this->assertNotNull($errors['name/company']);
         $this->assertNotNull($errors['who']);
         $this->assertNotNull($errors['address']);
